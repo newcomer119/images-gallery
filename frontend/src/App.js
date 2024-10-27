bee846817838d19cbb811d3c7758875d720e8eb3
newcomer119 <mitarth.125035@stu.upes.ac.in>
2024-10-28 02:46:13 +0530

    save images in the commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
-import { useState } from 'react'
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Header from './Components/Header'
-import Search from './Components/Search'
+import { useState } from "react";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Components/Header";
+import Search from "./Components/Search";
+
+// const UNSPALASH_API = process.env.REACT_UNSPLASH_API_KEY; THIS WAS NOT WORKING I TRIED TO CORRECT IT 
 
 const App = () => {
+  const [word, setWord] = useState("");
+  const[images, setImages] = useState("")
 
-  const [word,setWord] = useState('');
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-  }
-    
-  return(
-    <div className="App">
-      < Header title="Images Gallery"/>
-      < Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
-    </div>
-  )
-}
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=EMJtBuNU7ngIkcA_AdS0rqJVofAHQVXxqdKIbxou66c`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages([data, ...images]);
+        console.log(images);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      setWord('')
+  };
 
-export default App
+  return (
+    <div className="App">
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+    </div>
+  );
+};
+
+export default App;
