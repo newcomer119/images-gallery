1792c61cf1fa817f2bfa1fd3d5a31c4322892e8e
newcomer119 <mitarth.125035@stu.upes.ac.in>
2024-10-29 22:25:10 +0530

    added svg

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Navbar , Container} from "react-bootstrap";
+import { ReactComponent as Logo} from '../images/logo.svg'
 
-// const navbarStyle = {
-//     backGroundColor : 'lightblue'
-// };
 
 const Header = ({ title }) => {
   return (
     <Navbar bg="primary" data-bs-theme="light">
         <Container>
-            <Navbar.Brand href="/">{title}</Navbar.Brand>
+            {/* <Navbar.Brand href="/">{title}</Navbar.Brand> */}
+            <Logo style={{ maxWidth : '20rem', maxHeight : '4rem'}}/>
         </Container>
     </Navbar>
   );
