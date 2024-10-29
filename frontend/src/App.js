806b27e4105d0fe4eb8f2cb1a9a85c8ee2bf7125
newcomer119 <mitarth.125035@stu.upes.ac.in>
2024-10-29 22:05:25 +0530

    display and delete functionality added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Components/Header";
 import Search from "./Components/Search";
+import ImageCard from "./Components/ImageCard";
+import { Container, Row, Col } from "react-bootstrap";
 
-// const UNSPALASH_API = process.env.REACT_UNSPLASH_API_KEY; THIS WAS NOT WORKING I TRIED TO CORRECT IT 
+// const UNSPALASH_API = process.env.REACT_UNSPLASH_API_KEY; THIS WAS NOT WORKING I TRIED TO CORRECT IT
+// ... existing code ...
 
 const App = () => {
   const [word, setWord] = useState("");
-  const[images, setImages] = useState("")
+  const [images, setImages] = useState([]); // Initialize as empty array instead of empty string
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -18,21 +21,38 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images]);
+        setImages([{ ...data, title: word }, ...images]);
         console.log(images);
       })
       .catch((err) => {
         console.log(err);
       });
-      setWord('')
+    setWord("");
   };
+
+  const handleDeleteImage = (id) => {
+      setImages(images.filter((image) => image.id !== id))
+  }
 
   return (
     <div className="App">
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      {/* THIS CLASS MT-4 GIVES MARGIN T MEANS TOP IN BOOTSTRAP */}
+      <Container className="mt-4"> 
+        <Row xs={1} md={2} lg={3}>
+          {images &&
+            images.map((image, i) => (
+              <Col key={i} className="pb-4">  
+                <ImageCard image={image} deleteImage={handleDeleteImage}/> 
+              </Col>
+            ))}
+        </Row>
+      </Container>
     </div>
   );
 };
 
 export default App;
+
+// ... existing code ...
